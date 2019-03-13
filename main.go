9bcdcdf287eeac58272640784a4588117948d886
Paul Rapulu <rapulu@yahoo.com>
2019-03-13 22:17:22 +0100

    fix: url validation

--- main.go
+++ main.go
@@ -8,5 +8,10 @@ import(
 func main(){
 	url := flag.String("url", "", "Youtube Url")
 	flag.Parse()
+	
+	if *url == ""{
+	  fmt.Println("Please flag url should not be empty.")
+	  return
+	}
 	fmt.Printf("User input is: %s \n", *url)
 }
